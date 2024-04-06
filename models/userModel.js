aec1bec50fb0322fd2372be83d77031abf3f67c3
Anjitha Mohanan <113471696+AnjithaMohanan@users.noreply.github.com>
2024-04-06 10:07:58 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -3,20 +3,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String,required:true},
-    email: { type: String,required:true},
-    mobile: { type: String,required:true},
-    password: { type: String,required:true},
-    image:{ type: String,required:true},
-    is_admin:{ type:Number,required:true},
-    is_verified:{
-        type:Number,
-        default:0
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    mobile: { 
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^[6-9]\d{9}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid mobile number!`
+        }
     },
-    token:{
-        type:String,
-        default:''
-    }
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    is_admin: { type: Number, required: true },
+    is_verified: { type: Number, default: 0 },
+    token: { type: String, default: '' },
 });
 
 module.exports = mongoose.model('User', userSchema);
